a25239dd1d74cdf19efc6958bec73fb742478e4f
wwaltb <wwaltb@proton.me>
2025-02-03 02:14:40 -0800

    xdg userDirs

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,11 +1,13 @@
 {lib, ...}: {
-  imports = [
-    ./firefox
-    ./hypr
-    ./nvim
-  ];
+    imports = [
+        ./firefox
+        ./hypr
+        ./nvim
+        ./xdg.nix
+    ];
 
-  firefox.enable = lib.mkDefault true;
-  hyprland.enable = lib.mkDefault true;
-  nvim.enable = lib.mkDefault true;
+    firefox.enable = lib.mkDefault true;
+    hyprland.enable = lib.mkDefault true;
+    nvim.enable = lib.mkDefault true;
+    xdg.enable = lib mkDefault true;
 }
