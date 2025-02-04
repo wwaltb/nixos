64fbf8d98184f1cbb021af01d6d144af5da9ffb3
wwaltb <wwaltb@proton.me>
2025-02-03 20:30:36 -0800

    firefox css working

--- modules/home-manager/firefox/default.nix
+++ modules/home-manager/firefox/default.nix
@@ -13,10 +13,10 @@
             enable = true;
             profiles.default = {
                 isDefault = true;
-            };
             settings = {
                 # Enable userChrome customizations
                 "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+            };
             };
         };
         cascade.enable = true;
--- modules/home-manager/hypr/default.nix
+++ modules/home-manager/hypr/default.nix
@@ -1,56 +1,55 @@
 { config, lib, pkgs, inputs, ... }:
 { 
-    imports = [
-        ./keys.nix
-    ];
+        imports = [
+                ./keys.nix
+        ];
 
-	options = {
-		hyprland.enable = lib.mkEnableOption "enables hyprland";
-	};
-
-	config = lib.mkIf config.hyprland.enable {
-        programs.hyprland.enable = true;
-		wayland.windowManager.hyprland = {
-			enable = true;
-			settings = with config.colorScheme.palette; {
-				general = {
-					"col.active_border" = "rgba(${base06}ff)";
-					"col.inactive_border" = "rgba(${base03}ff)";
-                    border_size = 3;
-                    gaps_in = 4;
-                    gaps_out = 8;
-				};
-
-                decoration = {
-                    rounding = 4;
-                    rounding_power = 4;
-
-                    dim_special = 0.6;
-
-                    blur = {
-                        enabled = false;
-                    };
-                    shadow = {
-                        enabled = false;
-                    };
-                };
-
-                animations = {
-                    enabled = false;
-                };
-
-                cursor = {
-                    inactive_timeout = 0.5;
-                };
-
-                misc = {
-                    disable_hyprland_logo = true;
-                    force_default_wallpaper = 0;
-                    background_color = "0x${base00}";
-                };
-            };
-            systemd.variables = ["--all"];
+        options = {
+                hyprland.enable = lib.mkEnableOption "enables hyprland";
         };
-        hypr-keys.enable = true;
-    };
+
+        config = lib.mkIf config.hyprland.enable {
+                wayland.windowManager.hyprland = {
+                        enable = true;
+                        settings = with config.colorScheme.palette; {
+                                general = {
+                                        "col.active_border" = "rgba(${base06}ff)";
+                                        "col.inactive_border" = "rgba(${base03}ff)";
+                                        border_size = 3;
+                                        gaps_in = 4;
+                                        gaps_out = 8;
+                                };
+
+                                decoration = {
+                                        rounding = 4;
+                                        rounding_power = 4;
+
+                                        dim_special = 0.6;
+
+                                        blur = {
+                                                enabled = false;
+                                        };
+                                        shadow = {
+                                                enabled = false;
+                                        };
+                                };
+
+                                animations = {
+                                        enabled = false;
+                                };
+
+                                cursor = {
+                                        inactive_timeout = 0.5;
+                                };
+
+                                misc = {
+                                        disable_hyprland_logo = true;
+                                        force_default_wallpaper = 0;
+                                        background_color = "0x${base00}";
+                                };
+                        };
+                        systemd.variables = ["--all"];
+                };
+                hypr-keys.enable = true;
+        };
 }
