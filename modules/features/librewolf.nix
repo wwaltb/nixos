60e10cbe3d9d8f8e952a5169fd671212c11d7117
wwaltb <wwaltb@proton.me>
2026-07-06 00:40:34 -0700

    librewolf search engines

--- modules/features/librewolf.nix
+++ modules/features/librewolf.nix
@@ -1,15 +1,15 @@
-{
-  self,
-  inputs,
-  ...
-}: {
+{self, ...}: {
   flake.modules.nixos.librewolf = {
     home-manager.users.walt.imports = [
       self.modules.homeManager.librewolf
     ];
   };
 
-  flake.modules.homeManager.librewolf = {pkgs, ...}: {
+  flake.modules.homeManager.librewolf = {
+    config,
+    pkgs,
+    ...
+  }: {
     programs.librewolf = {
       enable = true;
       profiles.walt = {
@@ -18,6 +18,88 @@
           "sidebar.revamp" = true;
           "sidebar.verticalTabs" = true;
           "browser.toolbars.bookmarks.visibility" = "never"; # "newtab"
+
+          "privacy.sanitize.sanitizeOnShutdown" = "false";
+        };
+
+        search = {
+          force = true;
+          default = "ddg";
+          privateDefault = "ddg";
+
+          engines = {
+            "Nix Packages" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "channel";
+                      value = "unstable";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@np"];
+            };
+
+            "Nix Options" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/options";
+                  params = [
+                    {
+                      name = "channel";
+                      value = "unstable";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@no"];
+            };
+
+            "NixOS Wiki" = {
+              urls = [
+                {
+                  template = "https://wiki.nixos.org/w/index.php";
+                  params = [
+                    {
+                      name = "search";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@nw"];
+            };
+
+            "youtube" = {
+              urls = [
+                {
+                  template = "https://youtube.com/results";
+                  params = [
+                    {
+                      name = "search_query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              # icon = "https://brand.youtube/youtube-icon/#core-icon";
+              definedAliases = ["@yt"];
+            };
+          };
         };
 
         extensions = {
@@ -28,5 +110,8 @@
 
     stylix.targets.librewolf.profileNames = ["walt"];
     stylix.targets.librewolf.colorTheme.enable = true;
+    stylix.targets.librewolf.colors.override = {
+      base01 = config.lib.stylix.colors.base00;
+    };
   };
 }
