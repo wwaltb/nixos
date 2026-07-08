71de464fe04c1325515e990a61a11fb1efcb7d06
wwaltb <wwaltb@proton.me>
2026-07-07 21:27:24 -0700

    update general module

--- modules/features/general/system.nix
+++ modules/features/general/system.nix
@@ -1,12 +1,25 @@
 {...}: {
   flake.modules.nixos.general = {pkgs, ...}: {
     nix.settings.experimental-features = ["nix-command" "flakes"];
+    nixpkgs.config.allowUnfree = true;
+
+    boot.loader = {
+      efi.canTouchEfiVariables = true;
+
+      grub = {
+        enable = true;
+        efiSupport = true;
+        device = "nodev";
+        gfxmodeEfi = "1920x1080";
+        # gfxpayloadEfi = "text";
+      };
+
+      systemd-boot.enable = false;
+    };
 
     environment.systemPackages = with pkgs; [
       wget
       git
     ];
-
-    networking.networkmanager.enable = true;
   };
 }
--- modules/features/general/user.nix
+++ modules/features/general/user.nix
@@ -12,25 +12,8 @@
       ];
 
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519
-        AAAAC3NzaC1lZDI1NTE5AAAAIAxBwwcZ/KwY7UdVxk9vihml3+uFWAhy+c8mmT4qMGP+
-        walt@jonsbo6"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAxBwwcZ/KwY7UdVxk9vihml3+uFWAhy+c8mmT4qMGP+ walt@jonsbo6"
       ];
-    };
-
-    xdg.userDirs = {
-      enable = true;
-      createDirectories = true;
-      setSessionVariables = false;
-      desktop = "${config.home.homeDirectory}/desktop";
-      documents = "${config.home.homeDirectory}/documents";
-      download = "${config.home.homeDirectory}/downloads";
-      music = "${config.home.homeDirectory}/music";
-      pictures = "${config.home.homeDirectory}/pictures";
-      projects = "${config.home.homeDirectory}/projects";
-      publicShare = "${config.home.homeDirectory}/public";
-      templates = "${config.home.homeDirectory}/templates";
-      videos = "${config.home.homeDirectory}/videos";
     };
   };
 }
