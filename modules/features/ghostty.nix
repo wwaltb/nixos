e86d82070e0c22bd31fab19429eed97e9e870eec
wwaltb <wwaltb@proton.me>
2026-07-08 00:33:32 -0700

    ghostty deamon restarts to run all the time now

--- modules/features/ghostty.nix
+++ modules/features/ghostty.nix
@@ -6,9 +6,12 @@
   };
 
   flake.modules.homeManager.ghostty = {
+    config,
+    lib,
+    ...
+  }: {
     programs.ghostty = {
       enable = true;
-      systemd.enable = true;
       settings = {
         window-decoration = false;
         window-padding-x = "4";
@@ -16,6 +19,30 @@
         resize-overlay = "never";
         working-directory = "home";
         shell-integration = "detect";
+      };
+      systemd.enable = false;
+    };
+
+    systemd.user.services."app-com.mitchellh.ghostty" = {
+      Unit = {
+        Description = "Ghostty";
+        After = [
+          "graphical-session.target"
+          "dbus.socket"
+        ];
+        Requires = "dbus.socket";
+        X-RestartIfChanged = false;
+      };
+      Service = {
+        Type = "dbus";
+        BusName = "com.mitchellh.ghostty";
+        ReloadSignal = "SIGUSR2";
+        ExecStart = "${lib.getExe config.programs.ghostty.package} --gtk-single-instance=true --initial-window=false";
+        Restart = "always";
+        RestartSec = 1;
+      };
+      Install = {
+        WantedBy = ["graphical-session.target"];
       };
     };
   };
